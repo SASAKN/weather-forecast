e1da13a8e3327fb9e912a9a6de0a0f842500a482
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-02-11 18:37:17 +0900

    ベースURL1も追加

--- forecast/data_utils/download.py
+++ forecast/data_utils/download.py
@@ -8,20 +8,23 @@ place_code_2 = ['47646', '47600'] #気圧などのコード
 place_name_2 = ["館野", "輪島"] #気圧などの名前
 hours_list_2 = [9, 21] #気圧をとる時間
 
-# ベースとなるURL
-#year = 年, month = 月, day = 日, hour = 時 (9, 21, 3, 15), point = 地域コード
-base_url = "https://www.data.jma.go.jp/stats/etrn/upper/view/hourly_usp.php?year=%s&month=%s&day=%s&hour=%s&atm=&point=%s&view="
+# ベースURL1  prec_no = 都道府県コード, block_no = 地域コード, year = 年, month = 月, day = 日
+base_url_3 = "https://www.data.jma.go.jp/stats/etrn/view/hourly_s1.php?prec_no=%s&block_no=%s&year=%s&month=%s&day=%s&view="
 
+# ベースURL2 year = 年, month = 月, day = 日, hour = 時 (9, 21, 3, 15), point = 地域コード
+base_url_2 = "https://www.data.jma.go.jp/stats/etrn/upper/view/hourly_usp.php?year=%s&month=%s&day=%s&hour=%s&atm=&point=%s&view="
+
+#文字列を少数に変換
 def str2float(str):
     try:
         return float(str)
     except:
         return 0.0
 
-# 気圧などを取得する関数
+# ラジオゾンデ観測のデータをダウンロード
 def download_data_2():
     for place in place_name_2:
-        All_list = [['年月日', '気圧', '気温', '風速(m/s)', '風向(°)']] #集計データ
+        All_list_2 = [['年月日', '気圧', '気温', '風速(m/s)', '風向(°)']] #集計データ
         index = place.index(place)
         for year in range(2023, 2024):
             for month in range(1, 13):
@@ -31,7 +34,7 @@ def download_data_2():
                         print(str(year) + "/" + str(month) + "/" + str(date) + "/" + str(hour))
 
                         #URL作成
-                        req = requests.get(base_url%(year, month, date, hour, place_code_2[index]))
+                        req = requests.get(base_url_2%(year, month, date, hour, place_code_2[index]))
                         req.encoding = req.apparent_encoding
                         
                         #スクレイピング
@@ -52,11 +55,11 @@ def download_data_2():
                             row_data.append(str2float(data[5].string)) #風向
 
                         #まとめる
-                        All_list.append(row_data)
+                        All_list_2.append(row_data)
 
         with open(place + '.csv', 'w',encoding="utf_8_sig") as file: #文字化け防止
             writer = csv.writer(file, lineterminator='\n')
-            writer.writerows(All_list)
+            writer.writerows(All_list_2)
 
 if __name__ == "__main__":
     download_data_2()
