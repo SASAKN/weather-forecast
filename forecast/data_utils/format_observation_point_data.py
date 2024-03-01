02707b83ce5f9216550a0bdf408a31da4f36f25d
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-01 22:18:29 +0900

    地域番号も抜き出せた

--- forecast/data_utils/format_observation_point_data.py
+++ forecast/data_utils/format_observation_point_data.py
@@ -1,5 +1,14 @@
 import csv
 
+# 気象庁の観測地点コードは、5桁であるが、そのうち先頭2桁は、都道府県振興局番号である。
+# そして、都道府県振興局番号は、スクレイピングに必要なものである。
+def extract_first_two_digits_from_number(num):
+    if isinstance(num, int) and 9999 < num < 100000:
+        #スライス
+        return int(str(num)[:2])
+    else:
+        return 0
+
 def filter_csv(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8', newline='') as infile, \
          open(output_file, 'w', encoding='utf-8', newline='') as outfile:
@@ -14,7 +23,8 @@ def filter_csv(input_file, output_file):
         for row in reader:
             if len(row) >= 17 and row[2] == "官" and "気圧" not in row[15] and "気圧" not in row[16]:
                 writer.writerow(row)
-#いらないデータのカット
+
+# 観測地点一覧からいらないデータを削除
 def combine_columns(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8', newline='') as infile, \
          open(output_file, 'w', encoding='utf-8', newline='') as outfile:
@@ -23,12 +33,12 @@ def combine_columns(input_file, output_file):
 
         # ヘッダーを書き込む
         header = next(reader)
-        new_header = [header[0], header[1], header[7], header[8], header[9], header[10]]
+        new_header = ["都道府県振興局番号", header[1], header[7], header[8], header[9], header[10]]
         writer.writerow(new_header)
 
         # 指定された列を結合して新しい行を書き込む
         for row in reader:
-            new_row = [row[0], row[1], row[7], row[8], row[9], row[10]]
+            new_row = [str(extract_first_two_digits_from_number(int(row[1]))), row[1], row[7], row[8], row[9], row[10]]
             writer.writerow(new_row)
 
 # 使用例
