5bc815c33837382a2e83c3afb05ff4d4a51041cd
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-02 23:29:43 +0900

    かん前どうさ

--- test.py
+++ test.py
@@ -4,10 +4,20 @@ import csv
 import os
 import glob
 import pandas as pd
+import re
 
 #気象台都道府県コードは、11から94
 prec_codes = ['44']
-block_name = ['八王子', '東京']
+
+# ブロックコードのクラス
+class block_codes:
+    def __init__(self, prec, block, block_code):
+        self.prec = prec
+        self.block = block
+        self.block_code = block_code
+
+# クラスの配列
+class_array = []
 
 #ベースURL prec_no = 都道府県コード
 base_url = "https://www.data.jma.go.jp/stats/etrn/select/prefecture.php?prec_no=%s"
@@ -28,6 +38,15 @@ def column_to_array(input_file, column_index):
                 result_array.append(row[column_index])
 
     return result_array
+
+#Tagをコードに変換
+def tag2code(tag):
+    match = re.search(r"javascript:viewPoint\('.*?','(\d+)',", str(tag))
+    if match:
+        arg = match.group(1)
+        return arg
+    else:
+        return
 
 #都道府県コードにある地域名を出力
 def prec2block(prec_no, input_file):
@@ -103,8 +122,13 @@ def get_observation_points():
     for prec in prec_codes:
         #Indexを作成
         index = prec_codes.index(prec)
-        for block in block_name:
 
+        #処理中の都道府県を表示
+        print(f'処理中の都道府県振興局 : {prec_codes[index]}\n')
+
+        #地域の配列を作成
+        block_name = prec2block(prec_codes[index], 'filtered.csv')
+        for block in block_name:
             #Indexを作成
             index_2 = block_name.index(block)
 
@@ -117,10 +141,17 @@ def get_observation_points():
 
             #要素を検索してみつける
             tmp_maps = page.find_all('map', {'name': 'point'})
-            
-            tmp_area = tmp_maps[0].find_all('area', {'alt': block_name[index_2]})
-            print(tmp_area)
 
+            # エラーハンドリング
+            if tmp_maps == []:
+                print(f'[ INFO ]Prec None {prec_codes[index]}')
+            else:      
+                tmp_area = tmp_maps[0].find_all('area', {'alt': block_name[index_2]})
+                if tmp_area == []:
+                    print(f'[ INFO ]Block None {block_name[index_2]}')
+                else:
+                    print(tag2code(tmp_area[0]))
+                    class_array.append(block_codes(prec_codes[index], block_name[index_2], tag2code(tmp_area[0])))
 
 
 
@@ -146,8 +177,13 @@ if __name__ == "__main__":
     #重複したものを削除
     delete_duplicates(tmp2_file_path ,tmp3_file_path)
 
-    #都道府県TO地域
-    print(prec2block('11' ,tmp3_file_path))
+    #都道府県コードをまとめる
+    prec_codes = list(set(column_to_array(tmp3_file_path, 0)))
 
-    # #地域コードをJMAからスクレイピング
-    # get_observation_points()
+    #都道府県TO地域
+    print(prec2block(11 ,tmp3_file_path))
+
+    #地域コードをJMAからスクレイピング
+    get_observation_points()
+
+    print(f'{class_array[0].prec},{class_array[0].block},{class_array[0].block_code}')
--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import re
 
-text = "javascript:viewPoint('s','47678','八丈島','ハチジヨウ','33','07.3','139','46.7','151.2','1','1','1','1','1','1','9999','99','99','','','','','');"
+text = """<area alt="宇都宮" coords="149,183,160,194" href="../index.php?prec_no=41&amp;block_no=47615&amp;year=&amp;month=&amp;day=&amp;view=" onmouseout="javascript:initPoint();" onmouseover="javascript:viewPoint('s','47615','宇都宮','ウツノミヤ','36','32.9','139','52.1','119.4','1','1','1','1','1','1','9999','99','99','','','','','');" shape="rect"/>"""
 
 # 正規表現を使用して数字を抜き出す
 match = re.search(r"javascript:viewPoint\('.*?','(\d+)',", text)
@@ -10,3 +10,4 @@ if match:
     print(second_argument)
 else:
     print("2番目の引数が見つかりませんでした。")
+
