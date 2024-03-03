57292c66b744fb4c7036448c64ad6711ee17c404
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-03 11:02:53 +0900

    順番の変更

--- forecast/data_utils/format_observation_point_data.py
+++ forecast/data_utils/format_observation_point_data.py
@@ -142,8 +142,17 @@ def get_observation_points():
         #地域の配列を作成
         block_name = prec2block(prec_codes[index], 'filtered.csv')
         for block in block_name:
+
             #Indexを作成
             index_2 = block_name.index(block)
+
+            #つくばと奥日光と南大東のための専用処理
+            if block_name[index_2] == "つくば":
+                block_name[index_2] = "つくば（館野）"
+            elif block_name[index_2] == "奥日光":
+                block_name[index_2] = "奥日光（日光）"
+            elif block_name[index_2] == "南大東":
+                block_name[index_2] = "南大東（南大東島）"    
 
             #URLにアクセス
             req = requests.get(base_url%(prec_codes[index]))
@@ -158,22 +167,13 @@ def get_observation_points():
             # エラーハンドリング
             if tmp_maps == []:
                 print(f'[ INFO ]Prec None {prec_codes[index]}')
-            else:
-                #つくばと奥日光と南大東のための専用処理
-                if block_name[index_2] == "つくば":
-                    block_name[index_2] = "つくば（館野）"
-                elif block_name[index_2] == "奥日光":
-                    block_name[index_2] = "奥日光（日光）"
-                elif block_name[index_2] == "南大東":
-                    block_name[index_2] = "南大東（南大東島）"
-                else:      
-                    tmp_area = tmp_maps[0].find_all('area', {'alt': block_name[index_2]})
-                    if tmp_area == []:
-                        print(f'[ INFO ]Block None {block_name[index_2]}')
-                    else:
-                        #タグから地域コードを抜き出し、配列にクラスごと入れる
-                        # print(tag2code(tmp_area[0]))
-                        class_array.append(block_codes(prec_codes[index], block_name[index_2], tag2code(tmp_area[0])))
+            else:      
+                tmp_area = tmp_maps[0].find_all('area', {'alt': block_name[index_2]})
+                if tmp_area == []:
+                    print(f'[ INFO ]Block None {block_name[index_2]}')
+                else:
+                    #タグから地域コードを抜き出し、配列にクラスごと入れる
+                    class_array.append(block_codes(prec_codes[index], block_name[index_2], tag2code(tmp_area[0])))
 
 #CSV書き込み
 def two_dimension_array2csv(two_dimensional_data, output_csv):
