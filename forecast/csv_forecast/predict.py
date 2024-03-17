0ee995268bde581c12698958361b9e52bd821880
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-17 13:20:28 +0900

    テストなどの変更

--- forecast/csv_forecast/predict.py
+++ forecast/csv_forecast/predict.py
@@ -1,8 +1,44 @@
+import os
+import glob
 import numpy as np
 from keras.models import Sequential
 from keras.layers import LSTM
 
-def make_model():
+def extract_filename(file_path):
+    file_name = os.path.basename(file_path)
+    
+    file_name_without_extension, _ = os.path.splitext(file_name)
+    
+    return file_name_without_extension
+
+def find_target_csv_files():
+    csvs = glob.glob("../data_utils/*.csv")
+
+    #地点などのファイルを除外
+    csvs.remove('../data_utils/ame_master.csv')
+    csvs.remove('../data_utils/amedas.csv')
+    csvs.remove('../data_utils/kousou_master.csv')
+
+    return csvs
+
+def load_np_arrays_from_npz(npz_file):
+    return np.load(npz_file)
+
+def find_target_array_from_file(arrays, keyword):
+    return arrays[str(keyword)]
+
+def build_model():
     #Modelを作成
     model = Sequential()
     model.add(LSTM(64, input_shape=(64, 64, 64)))
+
+if __name__ == "__main__":
+    csv_files = find_target_csv_files()
+    npz_arrays = load_np_arrays_from_npz('npz_data/weatther_data.npz')
+    #地点ごとに行う
+    for tmp in csv_files:
+        block_code = extract_filename(tmp)
+        array = npz_arrays[f'{block_code}']
+        
+
+
--- test3.py
+++ test3.py
@@ -1,5 +1,12 @@
 import numpy as np
 
-arr = np.array([[1, 2], [3, 4]])  # shape: (2, 2)
-reshaped_arr = arr.reshape((4))  # 新しい形状: (4,)
-print(reshaped_arr)
+# 仮想的なデータの例として、各地点の気圧配置データが1時間ごとの2次元配列として与えられるとします
+# data_point_A, data_point_B, data_point_C はそれぞれ地点A、地点B、地点Cの気圧配置データを表します
+data_point_A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # 地点Aの気圧配置データ
+data_point_B = [[10, 11, 12], [13, 14, 15], [16, 17, 18]]  # 地点Bの気圧配置データ
+data_point_C = [[19, 20, 21], [22, 23, 24], [25, 26, 27]]  # 地点Cの気圧配置データ
+
+# 各地点のデータを横に連結して1つのデータセットに結合する
+combined_data = np.stack([data_point_A, data_point_B, data_point_C]).T
+
+print(combined_data)
