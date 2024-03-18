5d2f0255efe1a950996028529f92db6d9c1401b8
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-18 15:33:43 +0900

    データセットを作成するものを作成

---
+++ forecast/csv_forecast/create_dataset.py
@@ -0,0 +1,4 @@
+import numpy as np
+import json
+import glob
+import os
--- forecast/csv_forecast/predict.py
+++ forecast/csv_forecast/predict.py
@@ -2,14 +2,19 @@
 import os
 import glob
 import numpy as np
+import json
+from tqdm import tqdm
 
 #機械学習
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import LSTM
 
 #変数
 region_codes = []
 region_data = []
+
+#関数
 
 def extract_filename(file_path):
     file_name = os.path.basename(file_path)
@@ -35,23 +40,28 @@ def find_target_array_from_file(arrays, keyword):
     return arrays[str(keyword)]
 
 if __name__ == "__main__":
+    #メッセージを表示
+    print(f'Weather Predictor Alpha v0.1')
+
     #ファイルの読み込み
-    region_codes = (load_np_arrays_from_npz('./npz_data/block_list.npz'))['block'].tolist()
+    region_codes = (load_np_arrays_from_npz('./npz_data/block_list.npz'))['block']
     npz_file = load_np_arrays_from_npz('./npz_data/weather_data.npz')
 
-    print(region_codes)
+    #地域コードを一覧表示
+    print(f'気象庁地域コード : {region_codes.tolist()}')
 
-    print(npz_file)
+    #UNIX時間と対応があるデータを作成
+    all_array = []
 
+    #指数表記禁止
+    np.set_printoptions(suppress=True)
 
+    for array_key in tqdm(list(npz_file.keys())):
+        array = npz_file[f'{array_key}'].tolist()
+        all_array.append(array)
     
-    
+    print(all_array[0][0][0])
 
-
-
-
-    
-    
 
 
 
