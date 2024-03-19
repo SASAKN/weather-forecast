8ed9a3d96cf22a0ce87612003361caf443c7a399
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-19 21:58:39 +0900

    地域コードを場所に変換する関数を書いた

--- forecast/csv_forecast/predict.py
+++ forecast/csv_forecast/predict.py
@@ -4,6 +4,7 @@ import glob
 import numpy as np
 import json
 from tqdm import tqdm
+import csv
 
 #機械学習
 import tensorflow as tf
@@ -11,10 +12,21 @@ from keras.models import Sequential
 from keras.layers import LSTM
 from sklearn.model_selection import train_test_split
 
+#地点コードから緯度,経度
+def block2location(block_code, input_file):
+    result_array = []
+
+    with open(input_file, 'r', encoding='utf-8', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) > 0 and row[1].strip() == str(block_code):
+                result_array.append(row[3].strip(), row[4].strip())
+    return result_array
+
 if __name__ == "__main__":
 
     #2つのデータセットを読み込む
-    data_array = np.load('./npz_data/dataset.npz')['dataset_1']
+    data_array = np.load('./npz_data/dataset.npz')['dataset']
     block_array = np.load('./npz_data/block_list.npz')['block']
 
     #XとYのデータを指定
