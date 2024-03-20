1a90e76c64bfb416f3c8e3ce02535997af48cb2f
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-20 23:16:05 +0900

    datasetの作成

--- forecast/csv_forecast/create_dataset_2.py
+++ forecast/csv_forecast/create_dataset_2.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 if __name__ == "__main__":
+    np.set_printoptions(precision=3,suppress=True)
     array = np.load('./npz_data/dataset.npz')
-    np.set_printoptions(suppress=False, precision=2)
     print(array['dataset'])
-    print(array['dataset'].dtype)
