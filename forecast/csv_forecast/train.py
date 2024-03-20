30dbbff7807c0e786086d3584dbc419975295986
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-20 23:28:53 +0900

    ToDo　時系列でXとYでトレーニングデータを分ける

--- forecast/csv_forecast/train.py
+++ forecast/csv_forecast/train.py
@@ -10,11 +10,13 @@ def load_ndarray(npz_file):
 #モデルを作成して保存
 def save_model():
     model = Sequential()
-    model.add(ConvLSTM2D(64, (3, 3), input_shape=(1295136, 13))) #input_shapeには、画像の場合だと(width, height, 3)で良い
+    model.add(ConvLSTM2D(64, (3, 3), input_shape=(155, 215808, 12))) #input_shapeには、画像の場合だと(width, height, 3)で良い
     
     model.save('./model_data/model.h5', overwrite=True, save_format="h5")
 
 #メイン処理
 if __name__ == "__main__":
+    # 時系列でXとYに分ける
+
     save_model()
     
