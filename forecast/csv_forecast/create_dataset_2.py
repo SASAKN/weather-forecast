23511b1d3a9fb9fcca57cb7bce9d48f47b5c9371
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-21 23:24:14 +0900

    これで終わり

--- forecast/csv_forecast/create_dataset_2.py
+++ forecast/csv_forecast/create_dataset_2.py
@@ -1,16 +1,18 @@
 import numpy as np
 import pandas as pd
 
-def fix_cloudiness(row_idx, col_idx, target_array):
+def fix_cloudiness(row_idx, col_idx, target_array, target_number):
     index = 0
 
     while True:
         try:
-            if (col_idx - index) >= 0:
+            if (col_idx - index) <= 0 and index <= target_number:
                 if index == 0 and target_array[row_idx, col_idx, 2] <= 10:
                     break
                 elif index != 0 and target_array[row_idx, (col_idx - index), 2] <= 10 and target_array[row_idx, (col_idx + index), 2] <= 10:  # 改変が必要である場合
                     target_array[row_idx, col_idx, 2] = (target_array[row_idx, (col_idx - index), 2] + target_array[row_idx, (col_idx + index), 2]) / 2
+                    break
+                elif index > target_number:
                     break
         except Exception:
             index += 1
@@ -18,7 +20,7 @@ def fix_cloudiness(row_idx, col_idx, target_array):
 
         index += 1
 
-    result_array = target_array[row_idx, col_idx, 2]
+    result_array = target_array
     return result_array
 
 
@@ -32,11 +34,12 @@ if __name__ == "__main__":
     #11以上は外れ値なので修正
     print('雲量(Cloud)を修正します。')
     outliner_cloud_index = np.argwhere(array[:, :, 2] > 10)
-    outliner_cloud_array = array[outliner_cloud_index[:, 0], outliner_cloud_index[:, 1], 2]
+    outliner_cloud_number = len(outliner_cloud_index)
+    print(outliner_cloud_number)
+
 
     #外れ値を修正
     for outliner_cloud_idx in outliner_cloud_index:
         row_idx, col_idx = outliner_cloud_idx[0], outliner_cloud_idx[1]
-        print(outliner_cloud_index.shape)
-        fix_cloudiness(row_idx, col_idx, array)
+        fix_cloudiness(row_idx, col_idx, array, outliner_cloud_number)
         print(array[row_idx, col_idx, 2])
