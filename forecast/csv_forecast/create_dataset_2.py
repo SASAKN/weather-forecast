e3169d8b9a74c84c9a75749294155319c57d462c
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-21 21:42:11 +0900

    外れ値補完プログラムを作成しようとしている

--- forecast/csv_forecast/create_dataset_2.py
+++ forecast/csv_forecast/create_dataset_2.py
@@ -2,6 +2,5 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
-    np.set_printoptions(threshold=100, suppress=True)
-    pd.options.display.max_columns = 100
     array = np.load('./npz_data/dataset.npz')['dataset']
+    
