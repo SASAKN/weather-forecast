f36e254eabca03522baa1634a6fd5f641b9e07d1
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-23 20:59:21 +0900

    モデルを3使いた

--- forecast/csv_forecast/create_dataset_2.py
+++ forecast/csv_forecast/create_dataset_2.py
@@ -1,12 +1,25 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
-import pandas as pd
-import copy
-import warnings
 
+
+# 線形回帰
 def build_linear_model(x, y):
     model = LinearRegression()
+    model.fit(x, y)
+    return model
+
+# ロジスティック回帰
+def build_logistic_model(x, y):
+    model = LogisticRegression(random_state=0)
+    model.fit(x, y)
+    return model
+
+# ランダムフォレスト
+def build_random_forest_model(x, y):
+    model = RandomForestRegressor(n_estimators=100, random_state=40)
     model.fit(x, y)
     return model
 
@@ -14,43 +27,14 @@ if __name__ == "__main__":
     # データセットの読み込み
     dataset = np.load('./npz_data/dataset.npz')['dataset']
 
-    # 外れ値を含む配列と含まない配列を作成
+    # 外れ値のIndex, 正常値のIndex
     outliers = np.where(dataset[:, :, 2] > 10)
     inliers = np.where(dataset[:, :, 2] <= 10)
 
-    # 外れ値のインデックスをランダムに選択
-    outlier_indices = np.random.choice(len(outliers[0]), size=1000, replace=False)
+    # 外れ値と正常値の配列
+    outliers_array = dataset[outliers[0], outliers[1], 2]
+    inliers_array = dataset[inliers[0], inliers[1], 2]
 
-    # トレーニングデータを作成
-    x_train = dataset[inliers[0], inliers[1], 2].reshape(-1, 1)
-    y_train = dataset[inliers[0], inliers[1], 2]
+    
 
-    # 外れ値を含むデータを除外
-    x_train = np.delete(x_train, outlier_indices, axis=0)
-    y_train = np.delete(y_train, outlier_indices)
 
-    # データの正規化
-    scaler_x = StandardScaler()
-    scaler_y = StandardScaler()
-    x_train_scaled = scaler_x.fit_transform(x_train)
-    y_train_scaled = scaler_y.fit_transform(y_train.reshape(-1, 1))
-
-    # 線形回帰モデルを作成
-    model = build_linear_model(x_train_scaled, y_train_scaled)
-
-    # テストデータを作成
-    x_test = dataset[outliers[0], outliers[1], 2].reshape(-1, 1)
-    x_test_scaled = scaler_x.transform(x_test)  # テストデータも同様にスケーリング
-
-    # モデルを使って予測
-    y_pred_scaled = model.predict(x_test_scaled)
-
-    # 予測結果を元のスケールに戻す
-    y_pred = scaler_y.inverse_transform(y_pred_scaled)
-
-    # 予測結果を表示
-    print("予測結果:", y_pred)
-
-    # モデルのスコアを計算
-    score = model.score(x_test_scaled, y_pred_scaled)
-    print("Score:", score)
